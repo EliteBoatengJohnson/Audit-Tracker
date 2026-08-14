a9c102ca633574f586771c31a8359822189aec45
Boateng Johnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2026-08-14 10:54:17 +0000

    fix: minor changes

--- SecurityAuditTracker/SecurityAuditTracker/backend/SecurityAuditTracker.Api/obj/Debug/net10.0/SecurityAuditTracker.Api.AssemblyInfo.cs
+++ SecurityAuditTracker/SecurityAuditTracker/backend/SecurityAuditTracker.Api/obj/Debug/net10.0/SecurityAuditTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurityAuditTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d1959bceebf48a02574dd10ff6138a7c260a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b27251c5a5d0f0ec221f7237e932b5702d53a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurityAuditTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurityAuditTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
